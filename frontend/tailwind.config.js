d1aa12e78cbfcdd1b8731f58f668d3b501ecae87
Divinejay-001 <divinegiftjohnson21@gmail.com>
2025-05-23 00:24:18 -0700

    push

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#075cf5',
+        secondary: '#00c3c7',
+        tertiary: '#D22DC9',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif', "Quicksand"],
+        cinzel: ['Cinzel', 'serif'],
+
+      },
+     container: {
+       center: true,
+       padding:{
+        DEFAULT:'1rem',
+        sm: '2rem',
+        lg:'4rem',
+        xl:'5rem',
+        '2xl':'6rem',
+       }
+    }
   },
+},
   plugins: [],
 }
